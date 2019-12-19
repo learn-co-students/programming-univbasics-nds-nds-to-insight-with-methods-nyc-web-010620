3d35f21083ac393f41de7095ab8f78d8a81b909f
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-19 18:12:31 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,12 +1,28 @@
 require 'directors_database'
 
+
+
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
+  results = {}
+  count = 0
+  while count < nds.length
+    array.push(movies[count][:worldwide_gross])
+    count +=1
+  end
+
+ # result = {}
+ # movies = nds[]
+ # count = 0 
+ # while count < nds.length 
+   
+ # count += 1   
+  end
+  
   nil
 end
 
